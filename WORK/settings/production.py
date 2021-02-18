f17cd4a15607db0a9503f9669cccbf5ab84dc481
adilmathew <adilmathew@gmail.com>
2021-02-18 13:00:30 +0530

    asd

--- WORK/settings/production.py
+++ WORK/settings/production.py
@@ -145,5 +145,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 VENV_PATH = os.path.dirname(BASE_DIR)
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_files','static-root')
 STATICFILES_STORAGE =  'whitenoise.storage.CompressedManifestStaticFilesStorage'
